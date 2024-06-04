a5f64c1d12297f85c356db7c850258141690aa5f
yookyuhawn <yookyuhawn@naver.com>
2024-06-05 04:05:07 +0900

    change HomeController && homePage

--- src/main/java/OSS/Project/Controller/HomeController.java
+++ src/main/java/OSS/Project/Controller/HomeController.java
@@ -1,12 +1,29 @@
 package OSS.Project.Controller;
 
+import OSS.Project.domain.BoardJpa;
+import OSS.Project.domain.PostJpa;
+import OSS.Project.service.BoardService;
+import OSS.Project.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final BoardService boardService;
+    private final PostService postService;
+
     @GetMapping(value = "/")
-    static public String hello(){
+    public String hello(Model model){
+        List<BoardJpa> boardJpas = boardService.findAll();
+        for(int i=1; i<=boardJpas.size(); i++){
+            List<PostJpa> postJpas = postService.findByBoardId(boardJpas.get(i-1).getId(), 0, 3);
+            model.addAttribute("posts"+i, postJpas);
+        }
         return "home";
     }
 }
