7905f73b8cb4bf0cc0b60baf8a8f810cbc26973f
yookyuhawn <yookyuhawn@naver.com>
2024-06-04 17:44:25 +0900

    homepage Css and add board page

--- src/main/java/OSS/Project/Controller/HomeController.java
+++ src/main/java/OSS/Project/Controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping(value = "/")
     static public String hello(){
-        return "Home";
+        return "home";
     }
 }
